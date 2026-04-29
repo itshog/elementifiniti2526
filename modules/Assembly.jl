30f55d96435152e562133f0947657632ca48dd0f
itshog <itshog@protonmail.com>
2026-04-29 09:08:12 +0200

    Cose fatte in aula

--- modules/Assembly.jl
+++ modules/Assembly.jl
@@ -54,7 +54,7 @@ Compute the shape functions for the Poisson problem.
 # Returns
 - `shapef`: The shape functions evaluated at the quadrature points.
 """
-function shapef_2DLFE(quadrule::TriQuad)
+@memoize function shapef_2DLFE(quadrule::TriQuad)
     q = size(quadrule.points, 2)
     values = zeros(3,q)
 
@@ -76,7 +76,7 @@ Compute the gradients of the shape functions for the Poisson problem.
 # Returns
 - `∇shapef`: The gradients of the shape functions evaluated at the quadrature points.
 """
-function ∇shapef_2DLFE(quadrule::TriQuad)
+@memoize function ∇shapef_2DLFE(quadrule::TriQuad)
     q = size(quadrule.points, 2)
     A = [-1 1 0; -1 0 1]
     values = repeat(A, outer = [1,1,q])
@@ -101,7 +101,45 @@ Assemble the local stiffness matrix and force vector for the Poisson problem.
 - `fe`: The assembled local force vector.
 """
 function poisson_assemble_local!(Ke::Matrix, fe::Vector, mesh::Mesh, cell_index::Integer, f)
-    ###########################################################################
-    ############################ ADD CODE HERE ################################
-    ########################################################################### 
+    # Inizializzo a zero stiffness matrix e load vector
+    Ke = zeros(3,3)
+    fe = zeros(3)
+
+    # Seleziono formula di quadratura
+    quadrule = Q2_ref
+
+    # Numero punti di quadratura
+    q = size(quadrule.points,2)
+
+    # Dati della trasformazione affine per l'elemento di interesse della mesh
+    ak = mesh.ak[:,cell_index]
+    Bk = mesh.Bk[:,:,cell_index]
+    invBk = mesh.invBk[:,:,cell_index]
+
+    # Definisco i punti di quadratura sull'elemento reale applicando la trasformazione affine (pushforward) ai punti di quadratura sull'elemento di riferimento
+    pe = Bk * quadrule.points .+ ak # Valori sui punti, xp = Bk * hat xp + ak
+
+    # I valori delle funzioni sui punti di quadratura reali coincidono con i valori sui punti di quadratura dell'elemento di riferimento
+    phie = shapef_2DLFE(quadrule)
+
+    grad_phi = ∇shapef_2DLFE(quadrule)
+    grad_phie = invBk[cell_index] * grad_phi
+    # grad_phie = mult(invBk,grad_phi,axis=1)
+
+    # grad_phie = mult(invBk, ∇shapef_2DLFE(quadrule, axis=1)
+    
+    for p=1:q
+        wp = quadrule.weights[p] * abs(mesh.detBk[cell_index]) # wp = hat wp * |det Bk|
+        for i=1:3
+            v = phie[i,p]
+            grad_v = grad_phie[:,i,p]
+            fe[i] += wp * v * f(pe[:,p]) # Questo e Ke uniche cose che cambiano se implemento FEM per un problema diffusione trasporto o di altro tipo...
+            for j=1:3
+                grad_u = grad_phie[:,j,p]
+                Ke[j,i] += wp * grad_v * grad_u # Questo e fe uniche cose che cambiano se implemento FEM per un problema diffusione trasporto o di altro tipo...
+            end
+        end
+    end
+
+    return Ke, fe
 end
